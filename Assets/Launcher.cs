83e1afa76b388e417ae5cd6314144ea1b28b7de9
Julian Brasolin <julianbraso@gmail.com>
2020-09-23 16:32:44 -0300

    launcher

---
+++ Assets/Launcher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[System.Serializable]
+public class ConfigData{
+    public string configName;
+    public float[] configFloats;
+    public bool[] configBools;
+}
+
+public class Launcher : MonoBehaviour
+{
+    [SerializeField]
+    protected string execName;
+    
+    [SerializeField]
+    protected string execPath;
+
+    [SerializeField]
+    TMP_InputField nameField;
+
+    [SerializeField]
+    TMP_InputField floatsField;
+
+    [SerializeField]
+    TMP_InputField boolsField;
+
+    public void Launch(){
+        // var configFloats = floatsField.text.Split(","[0]);
+        // var configBools = boolsField.text.Split(","[0]);
+
+        ///////////////////////////////////////////////////////////////////CONFIG
+        var config = new ConfigData();
+        config.configName = nameField.text;
+        config.configFloats = Array.ConvertAll<string,float>(floatsField.text.Split(','), float.Parse);//boolsField.text.Split(","[0]);
+        config.configBools = Array.ConvertAll<string,bool>(boolsField.text.Split(','), bool.Parse);//boolsField.text.Split(","[0]);
+
+        string configJson = JsonUtility.ToJson(config);
+        System.IO.File.WriteAllText(Application.dataPath + "/../" +"launcher.cfg",configJson);
+        ///////////////////////////////////////////////////////////////////
+
+        if(execPath.Length > 0){
+             System.Diagnostics.Process.Start(execPath);
+             Application.Quit();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string appDataPath = Application.dataPath + "/../";
+        //Debug.Log(appDataPath);
+        execPath = Application.dataPath + "/../" + execName;
+        System.IO.File.WriteAllText(appDataPath+"verif","");
+        //System.IO.File.WriteAllText(Application.dataPath + "/../" +"verif",Application.dataPath);
+        System.IO.File.SetAttributes(appDataPath+"verif", System.IO.FileAttributes.Hidden);
+
+       
+
+        
+    }
+}
---
+++ Assets/LauncherCheck.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LauncherCheck : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI usernameText;
+
+    [SerializeField]
+    private GameObject boolGroup, floatGroup;
+
+    private ConfigData config;
+
+    public GameObject textPrefab; 
+
+    void Start()
+    {
+        string appDataPath = Application.dataPath + "/../";
+        string path = appDataPath+"verif";
+        bool exists = System.IO.File.Exists(path);
+        if (!exists){ 
+            Debug.Log("launcher did not found verif file!");
+            Application.Quit();
+        };
+        System.IO.File.Delete(path);
+
+        ///////////////////////////////CONFIG
+        string configPath = Application.dataPath + "/../" +"launcher.cfg";
+        bool configExists = System.IO.File.Exists(configPath);
+        if (!configExists){ 
+            Debug.Log("launcher did not found Config file!");
+            Application.Quit();
+        };
+        var source=new System.IO.StreamReader(configPath);
+        var fileContents=source.ReadToEnd();
+        source.Close();
+        config = JsonUtility.FromJson<ConfigData>(fileContents);
+        usernameText.text = config.configName;
+        foreach(bool b in config.configBools){
+            var e = Instantiate(textPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            e.GetComponent<TextMeshProUGUI>().text = b.ToString();
+            e.transform.SetParent(boolGroup.transform);
+        }
+        foreach(float f in config.configFloats){
+            var e = Instantiate(textPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            e.GetComponent<TextMeshProUGUI>().text = f.ToString();
+            e.transform.SetParent(floatGroup.transform);
+        }
+
+    }
+}
